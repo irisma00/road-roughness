95e2eaf7c8da69f663237f13de839c7d146cbf7a
Iris Ma <huaiyao@iastate.edu>
2022-04-24 16:27:39 -0500

    update

--- public/mode.js
+++ public/mode.js
@@ -3,3 +3,23 @@ const chk = document.getElementById('chk');
 chk.addEventListener('change', () => {
 	document.body.classList.toggle('dark');
 });
+
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth() + 1; //January is 0!
+var yyyy = today.getFullYear();
+
+if (dd < 10) {
+	dd = '0' + dd;
+ }
+ 
+ if (mm < 10) {
+	mm = '0' + mm;
+ } 
+	 
+todayDate = yyyy + '-' + mm + '-' + dd;
+todayMonth = yyyy + '-' + mm;
+document.getElementById("datePicker").setAttribute("max", todayDate);
+document.getElementById("monthPicker").setAttribute("max", todayMonth);
+ 
